abcd5bb9232b079a1fdbd384dab254da2d7a7b1d
unknown <jeisson.moscoso@ucp.edu.co>
2019-05-14 16:42:18 -0500

    insertar id

--- lista.php
+++ lista.php
@@ -4,13 +4,13 @@ if ($mysqli->connect_error) {
     exit('Could not connect');
 }
 
-$sql = "SELECT  Nombre, Apellido FROM alumnos WHERE Curso = ?";
+$sql = "SELECT  ID ,Nombre, Apellido FROM alumnos WHERE Curso = ?";
 
 $stmt = $mysqli->prepare($sql);
 $stmt->bind_param("s", $_GET['q']);
 $stmt->execute();
 $stmt->store_result();
-$stmt->bind_result($Nombre, $Apellido);
+$stmt->bind_result($ID, $Nombre, $Apellido);
 
 echo "<h3>Lista de alumnos</h3><br><div class='row'>
             <div class='col-md-12'>
@@ -18,6 +18,7 @@ echo "<h3>Lista de alumnos</h3><br><div class='row'>
 echo "<table class='table table-hover'>
     <thead>
 <tr>
+<th scope='col'>Documento</th>
 <th scope='col'>Nombre y Apellido</th>
 <th scope='col' style='
     text-align: center;
@@ -42,28 +43,30 @@ if ($stmt->num_rows > 0) {
 
         echo "<tbody>";
         echo "<tr>";
-        echo "<td>" . $Nombre . " " . $Apellido . "</td>
+        echo "
+        <td>" . $ID . "
+        <td>" . $Nombre . " " . $Apellido . "</td>
         <td>
-            <input class='form-check-input rasiatencia' type='radio' id='blabla".$i."' name='lsasito".$i."'>
-            <label class='form-check-label' for='blabla".$i."'>
+            <input class='form-check-input rasiatencia' type='radio' id='blabla" . $i . "' name='lsasito" . $i . "'>
+            <label class='form-check-label' for='blabla" . $i . "'>
                 Asistio
             </label>
         </td>
         <td>
-            <input class='form-check-input rasiatencia' type='radio' id='blas".$i."' name='lsasito".$i."'>
-            <label class='form-check-label' for='blas".$i."'>
+            <input class='form-check-input rasiatencia' type='radio' id='blas" . $i . "' name='lsasito" . $i . "'>
+            <label class='form-check-label' for='blas" . $i . "'>
                 No Asistio
             </label>
         </td>
         <td>
-            <input class='form-check-input ralmorzo' type='radio' id='almurzla".$i."' name='lalmuero".$i."'>
-            <label class='form-check-label' for='almurzla".$i."'>
+            <input class='form-check-input ralmorzo' type='radio' id='almurzla" . $i . "' name='lalmuero" . $i . "'>
+            <label class='form-check-label' for='almurzla" . $i . "'>
                 Si  Almorzo
             </label>
         </td>
         <td>
-            <input class='form-check-input ralmorzo' type='radio' id='almurzlano".$i."' name='lalmuero".$i."'>
-            <label class='form-check-label' for='almurzlano".$i."'>
+            <input class='form-check-input ralmorzo' type='radio' id='almurzlano" . $i . "' name='lalmuero" . $i . "'>
+            <label class='form-check-label' for='almurzlano" . $i . "'>
                 No Almorzo
             </label>
         </td>
@@ -73,7 +76,7 @@ if ($stmt->num_rows > 0) {
 
 
   </tr>";
-  $i+=1;
+        $i += 1;
     }
     echo "</tbody></table>";
     echo "</form></div><div  class='col-md-12'><button type='button' name='Guardar' class='btn btn-outline-success' onclick='sendasistent();'>Guardar</button></div>";
