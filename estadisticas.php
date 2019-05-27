4fd9b7130d09f1f3518e07b0419efed9578964f7
Monitor GTI <GTI@administrativos.local>
2019-05-27 18:03:43 -0500

    ya estadistica

--- estadisticas.php
+++ estadisticas.php
@@ -1,208 +1,146 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		
-		<title>Estadisticas</title>
 
-		<style type="text/css">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-		</style>
-	</head>
-	<body>
-<script src="Highcharts/code/highcharts.js"></script>
-<script src="Highcharts/code/modules/exporting.js"></script>
-<script src="Highcharts/code/modules/export-data.js"></script>
+    <title>Estadisticas</title>
 
+    <style type="text/css">
 
+    </style>
+</head>
 
-
-
-<div id="grados" style="width: 600px; height: 400px; margin: 0 auto; padding-right:900px; "></div>
-
-<div id="edad" style="width: 600px; height: 400px; margin: 0 auto; padding-right:900px; "></div>
-
-
-<div id="materias" style="width: 600px; height: 400px; margin: 0 auto; padding-left: 600px; margin-top: -800px;"></div>
-
+<body>
+    <script src="Highcharts/code/highcharts.js"></script>
+    <script src="Highcharts/code/modules/exporting.js"></script>
+    <script src="Highcharts/code/modules/export-data.js"></script>
+    <script>
+        var num6a;
+        var num7a;
+        var num8a;
+        var num9a;
+        var num10a;
+        var num11a;
+    </script>
 
 
 
-<?php
+
+
+    <div id="grados" style="width: 600px; height: 400px; margin: 0 auto; padding-right:900px; "></div>
+
+    <div id="edad" style="width: 600px; height: 400px; margin: 0 auto; padding-right:900px; "></div>
+
+
+    <div id="materias" style="width: 600px; height: 400px; margin: 0 auto; padding-left: 600px; margin-top: -800px;">
+    </div>
+
+
+
+
+    <?php
 
 include ("Conexion/connection.php");
 
+
+$consul6A = "SELECT COUNT(*) as numbery FROM asistencia  WHERE asistencia.Grado LIKE '%6A%' AND asistencia.Asistio = 0";
+$consul7A = "SELECT COUNT(*) as numbery FROM asistencia  WHERE asistencia.Grado LIKE '%7A%' AND asistencia.Asistio = 0";
+$consul8A = "SELECT COUNT(*) as numbery FROM asistencia  WHERE asistencia.Grado LIKE '%8A%' AND asistencia.Asistio = 0";
+$consul9A = "SELECT COUNT(*) as numbery FROM asistencia  WHERE asistencia.Grado LIKE '%9A%' AND asistencia.Asistio = 0";
+$consul10A = "SELECT COUNT(*) as numbery FROM asistencia  WHERE asistencia.Grado LIKE '%10A%' AND asistencia.Asistio = 0";
+$consul11A = "SELECT COUNT(*) as numbery FROM asistencia  WHERE asistencia.Grado LIKE '%11A%' AND asistencia.Asistio = 0";
+
+$query6A = $conn->query($consul6A);
+$query7A = $conn->query($consul7A);
+$query8A = $conn->query($consul8A);
+$query9A = $conn->query($consul9A);
+$query10A = $conn->query($consul10A);
+$query11A = $conn->query($consul11A);
+
+while ($row = $query6A->fetch_assoc()) {
+echo '<script> num6a = '.$row['numbery'].'</script>';
+}
+while ($row = $query7A->fetch_assoc()) {
+    echo '<script> num7a = '.$row['numbery'].'</script>';
+}
+while ($row = $query8A->fetch_assoc()) {
+    echo '<script> num8a = '.$row['numbery'].'</script>';
+}
+while ($row = $query9A->fetch_assoc()) {
+    echo '<script> num9a = '.$row['numbery'].'</script>';
+}
+while ($row = $query10A->fetch_assoc()) {
+    echo '<script> num10a = '.$row['numbery'].'</script>';
+}
+
+while ($row = $query11A->fetch_assoc()) {
+    echo '<script> num11a = '.$row['numbery'].'</script>';
+}
   ?>
-
-
-
-		<script type="text/javascript">
-Highcharts.chart('grados', {
-    chart: {
-        type: 'column'
-    },
-    title: {
-        text: 'Inasistencias por grado'
-    },
-    subtitle: {
-        text: ''
-    },
-    xAxis: {
-        type: 'category',
-        labels: {
-            rotation: -45,
-            style: {
-                fontSize: '13px',
-                fontFamily: 'Verdana, sans-serif'
-            }
-        }
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: ''
-        }
-    },
-    legend: {
-        enabled: false
-    },
-    tooltip: {
-        pointFormat: 'grados: <b>{point.y:.1f} millions</b>'
-    },
-    series: [{
-        name: 'Population',
-        data: [
-            ['6A', 24.2],
-            ['Beijing', 20.8],
-            ['Karachi', 14.9],
-            ['Shenzhen', 13.7],
-            ['Guangzhou', 13.1],
-            ['Istanbul', 12.7],
-            ['Mumbai', 12.4],
-            ['Moscow', 12.2],
-            ['São Paulo', 12.0],
-            ['Delhi', 11.7],
-            ['Kinshasa', 11.5],
-            ['Tianjin', 11.2],
-            ['Lahore', 11.1],
-            ['Jakarta', 10.6],
-            ['Dongguan', 10.6],
-            ['Lagos', 10.6],
-            ['Bengaluru', 10.3],
-            ['Seoul', 9.8],
-            ['Foshan', 9.3],
-            ['Tokyo', 9.3]
-        ],
-        dataLabels: {
-            enabled: true,
-            rotation: -90,
-            color: '#FFFFFF',
-            align: 'right',
-            format: '{point.y:.1f}', // one decimal
-            y: 10, // 10 pixels down from the top
-            style: {
-                fontSize: '13px',
-                fontFamily: 'Verdana, sans-serif'
-            }
-        }
-    }]
-});
-		</script>
-
-
-	<script>
-		
-
-Highcharts.chart('materias', {
-  chart: {
-    type: 'line'
-  },
-  title: {
-    text: 'Inasistencias por materia'
-  },
-  subtitle: {
-    text: ''
-  },
-  xAxis: {
-    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-  },
-  yAxis: {
-    title: {
-      text: ''
-    }
-  },
-  plotOptions: {
-    line: {
-      dataLabels: {
-        enabled: true
-      },
-      enableMouseTracking: false
-    }
-  },
-  series: [{
-    name: 'Tokyo',
-    data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-  }]
-});
-
-	</script>
-
-
-<script type="text/javascript">
-// Build the chart
-Highcharts.chart('edad', {
-    chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
-    },
-    title: {
-        text: 'Inasistencia por jornada unica	'
-    },
-    tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
+    <script type="text/javascript">
+        Highcharts.chart('grados', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Inasistencias por grado'
+            },
+            xAxis: {
+                type: 'category',
+                labels: {
+                    rotation: -45,
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: ''
+                }
+            },
+            legend: {
                 enabled: false
             },
-            showInLegend: true
-        }
-    },
-    series: [{
-        name: 'Brands',
-        colorByPoint: true,
-        data: [{
-            name: 'Chrome',
-            y: 61.41,
-            sliced: true,
-            selected: true
-        }, {
-            name: 'Internet Explorer',
-            y: 11.84
-        }, {
-            name: 'Firefox',
-            y: 10.85
-        }, {
-            name: 'Edge',
-            y: 4.67
-        }, {
-            name: 'Safari',
-            y: 4.18
-        }, {
-            name: 'Other',
-            y: 7.05
-        }]
-    }]
-});
-		</script>
+            tooltip: {
+                pointFormat: 'grados: <b>{point.y:.1f}  inasistencias</b>'
+            },
+            series: [{
+                name: 'Population',
+                data: [
+                    
+                    ['6A', num6a],
+                    ['7A', num7a],
+                    ['8A', num8a],
+                    ['9A', num9a],
+                    ['10A', num10a],
+                    ['11A', num11a]
+                   
+                ],
+                dataLabels: {
+                    enabled: true,
+                    rotation: -90,
+                    color: '#FFFFFF',
+                    align: 'right',
+                    format: '{point.y:.1f}', // one decimal
+                    y: 10, // 10 pixels down from the top
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            }]
+        });
+    </script>
+
+    
 
 
 
-	</body>
+</body>
+
 </html>
